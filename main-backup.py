3f6579d5f369cd87a6ca23e66e62f083d1b507b0
lewi9 <lewinski@student.agh.edu.pl>
2022-11-01 14:36:52 +0100

    Solver still needs fixes, added bushy maze

--- main-backup.py
+++ main-backup.py
@@ -18,22 +18,22 @@ def genMaze(x: int, y : int) -> np.array:
 
     def genRoad(a: int, b:int) -> None:
         l[a][b] = 2
-        direction: int = random.randrange(4)
+        t: int = random.randrange(4)
         increament: int = 0
         
         while increament < 4:
-            direction = (direction+increament)%4
-            if direction == 0:
+            t = (t+increament)%4
+            if t == 0:
                 if a+2 < y*2-1:
                     if l[a+2][b] == 1:
                         l[a+1][b] = 1
                         genRoad(a+2,b)
-            elif direction == 1:
+            elif t == 1:
                 if b+2 < x*2-1:
                     if l[a][b+2] == 1:
                         l[a][b+1] = 1
                         genRoad(a,b+2)
-            elif direction == 2:
+            elif t == 2:
                 if a-2 >= 0:
                     if l[a-2][b] == 1:
                         l[a-1][b] = 1
@@ -54,39 +54,18 @@ def genMaze(x: int, y : int) -> np.array:
 def randMaze(maze: np.array, x: int, y:int, p:float) -> np.array:
     l: np.array = np.copy(maze)
     count: int = np.count_nonzero(l==0)
-    wallToDestroy: int = int(random.randrange(count)*p)
-    while wallToDestroy > 0:
-        tx: int = random.randrange(x*2-1)
-        ty: int = random.randrange(y*2-1)
+    t: int = int(random.randrange(count)*p)
+    while t > 0:
+        tx = random.randrange(x*2-1)
+        ty = random.randrange(y*2-1)
         if l[ty][tx] == 0:
             l[ty][tx] = 1
-            wallToDestroy-= 1
+            t -= 1
     return l
-
-def bushyMaze(maze: np.array, x: int, y:int) -> np.array:
+                 
+def waterSplash(maze: np.array, x: int, y:int) -> np.array:
     l: np.array = np.copy(maze)
-    centerFactor: int = 4
-    outsideCenterFactor: int = 8
-    centerBorderY: tuple = (int((2*y-1)/centerFactor),int((2*y-1)/centerFactor*(centerFactor-1)))
-    centerBorderX: tuple = (int((2*x-1)/centerFactor),int((2*x-1)/centerFactor*(centerFactor-1)))
-    outsideCenterBorderY: tuple = (int((2*y-1)/outsideCenterFactor),int((2*y-1)/outsideCenterFactor*(outsideCenterFactor-1)))
-    outsideCenterBorderX: tuple = (int((2*x-1)/outsideCenterFactor),int((2*x-1)/outsideCenterFactor*(outsideCenterFactor-1)))
-    for i in range(centerBorderY[0], centerBorderY[1]):
-        for j in range(centerBorderX[0], centerBorderX[1]):
-            if l[i][j] > 0:
-                l[i][j] += 3
-    for i in range(outsideCenterBorderY[0], outsideCenterBorderY[1]):
-        for j in range(outsideCenterBorderX[0], outsideCenterBorderX[1]):
-            if l[i][j] > 0:
-                l[i][j] += 1
-    l[0][0] = 8
-    l[2*y-2][2*x-2] = 8
-    return l
-            
-def waterSplash(maze: np.array, x: int, y: int) -> np.array:
-    l: np.array = np.copy(maze)
-    l[l>1] = 1
-    l[2*y-2][2*x-2] = 2
+    l[0][0] = 1
     def water(w: int, z: int, p: int) -> None:
         if w+1 < 2*y-1:
             if l[w+1][z] == 1 or l[w+1][z] > l[w][z]+1:
@@ -106,7 +85,7 @@ def waterSplash(maze: np.array, x: int, y: int) -> np.array:
                 water(w,z-1,p+1)
 
     water(2*y-2,2*x-2,1)
-    l[2*y-2][2*x-2] = 0
+    l[2*y-2][2*x-2,] = 0
     return l
         
 def solveMaze(maze: np.array, waterSplashed: np.array, x:int, y:int) -> tuple:
@@ -189,7 +168,7 @@ def main():
 
     # Note that maze will be 2*x wide and 2*y height
     # IMPORTANT x*y should be not too much, because can be problem with shell and program execution (recursion)
-    x: int = 70
+    x: int = 80
     y: int = 30
 
     if x<3:
@@ -202,26 +181,20 @@ def main():
     n: int = 0
     nR: int = 0
     maze            = genMaze(x, y)
-    mazeR           = randMaze(maze, x, y, 0.5)
-    mazeRB          = bushyMaze(mazeR, x, y)
+    mazeR           = randMaze(maze, x, y, 0.1)
     waterSplashed   = waterSplash(maze, x, y)
     waterSplashedR  = waterSplash(mazeR, x, y)
-    waterSplashedRB = waterSplash(mazeRB, x, y)
     solution,  n    = solveMaze(maze, waterSplashed, x, y)
     solutionR, nR   = solveMaze(mazeR, waterSplashedR, x, y)
-    solutionRB, nRB = solveMaze(mazeRB, waterSplashedRB, x, y)
 
-    fig,ax=plt.subplots(3,3,figsize=(50,50))
+    fig,ax=plt.subplots(3,2,figsize=(50,50))
 
     showMaze(ax[0,0], "Maze.", maze, x, y)
     showMaze(ax[0,1], "Randomized maze.", mazeR, x, y)
-    showMaze(ax[0,2], "Randomized bushy maze. Walking by bush costs more.", mazeRB, x, y)
-    showMaze(ax[1,0], "Hueristic function (water splash).", waterSplashed, x, y)
-    showMaze(ax[1,1], "Hueristic function (water splash)\nof randomized maze.", waterSplashedR, x, y)
-    showMaze(ax[1,2], "Hueristic function (water splash)\nof randomized bushy maze.", waterSplashedRB, x, y)
-    showMaze(ax[2,0], "Solution of maze. Cost: " + str(n), solution, x, y)
-    showMaze(ax[2,1], "Solution of randomized maze. Cost: " + str(nR), solutionR, x, y)
-    showMaze(ax[2,2], "Solution of randomized bushy maze. Cost: " + str(nRB), solutionRB, x, y)
+    showMaze(ax[1,0], "Water splash of maze.", waterSplashed, x, y)
+    showMaze(ax[1,1], "Water splash of randomized maze.", waterSplashedR, x, y)
+    showMaze(ax[2,0], "Solution of maze. Steps: " + str(n), solution, x, y)
+    showMaze(ax[2,1], "Solution of randomized maze. Steps: " + str(nR), solutionR, x, y)
 
     fig.suptitle("Maze generator and solver. Start is in bottom-left corner, end is in top-right.", fontsize=18, fontweight='bold')
     
